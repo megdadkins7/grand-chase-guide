a0c524af698345f8faa34164649bc5ec51182bb4
Meghan Adkins <meghandadkins@gmail.com>
2019-09-18 13:34:22 -0400

    initial commit and setup

---
+++ src/components/HeroList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeroList = styled.div``;
+
+function HeroList() {
+  return (
+    <StyledHeroList>
+        Hello from hero list
+    </StyledHeroList>
+  )
+}
+
+export default HeroList;
